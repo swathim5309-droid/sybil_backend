8264ac7d05939cd142292d99d24211d538246e61
swathim5309-droid <swathim5309@gmail.com>
2026-02-08 19:59:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ app.add_middleware(
         "https://6000-firebase-studio-1765414312477.cluster-osvg2nzmmzhzqqjio6oojllbg4.cloudworkstations.dev",
         "https://6000-firebase-threatwatch-avdup-1767074125878.cluster-osvg2nzmmzhzqqjio6oojllbg4.cloudworkstations.dev",
         "https://6000-firebase-threatwatch-avdup1-1769254947663.cluster-osvg2nzmmzhzqqjio6oojllbg4.cloudworkstations.dev",
+        https://threat-watch-a-vdup1.vercel.app/,
     ],
     allow_credentials=True,
     allow_methods=["*"],
